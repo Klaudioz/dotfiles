63ee75d83b701f7c7e83401bc0e3e58566e1678d
klaudioz <klaudioz@gmail.com>
2026-03-15 14:25:46 -0300

    fix: stop sketchybar cpu runaway

--- sketchybar/plugins/cpu/script.sh
+++ sketchybar/plugins/cpu/script.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
+set -e
 
-CPU=$(top -l 1 -n 0 | awk '/^CPU usage:/ {
-  gsub(/%/, "")
-  user = $3
-  sys = $5
-  printf "%.0f", user + sys
-}')
+CORES=$(/usr/sbin/sysctl -n hw.logicalcpu 2>/dev/null || echo 1)
+
+CPU=$(
+  /bin/ps -A -o %cpu= |
+    /usr/bin/awk -v cores="$CORES" '
+      { sum += $1 }
+      END {
+        if (cores < 1) {
+          cores = 1
+        }
+
+        value = sum / cores
+        if (value < 0) {
+          value = 0
+        }
+        if (value > 100) {
+          value = 100
+        }
+
+        printf "%.0f", value
+      }
+    '
+)
 
 sketchybar --set "$NAME" label="${CPU}%"
